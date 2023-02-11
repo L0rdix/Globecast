1778556412d007ea97c2d08caa24c1627afa9fb0
Amir <96391496+L0rdix@users.noreply.github.com>
2023-02-11 15:28:42 +0100

    added checking for duplicates

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,6 @@
 let inputElement = document.querySelector('[data-input]');
+const list = document.getElementById('cities');
+const errorMsg = document.getElementById('error-msg');
 
 function GetWeather(){
   let inputVal = inputElement.value;
@@ -6,11 +8,34 @@ function GetWeather(){
   fetch(`http://api.openweathermap.org/data/2.5/weather?q=${inputVal}&APPID=692aecf0eca12b1e8b1b72c5e62db76f&units=metric`)
   .then(response => response.json())
   .then(data => {
-    console.log(data.weather);
-
     const icon = `https://openweathermap.org/img/wn/${data.weather[0]["icon"]}@2x.png`;
-
-    console.log(icon);
+    const listItems = list.querySelectorAll(".bottom .city");
+    const listItemsArray = Array.from(listItems);
+  
+    if(listItemsArray.length > 0){
+      const arrayFilter = listItemsArray.filter(el => {
+        let content = "";
+  
+        if(inputVal.includes(",")){
+          if(inputVal.split(",")[1].length > 2){
+            inputVal = inputVal.split(",")[0];
+            content = el.querySelector(".city-name span").textContent.toLowerCase();
+          }else{
+            content = el.querySelector(".city-name").textContent.toLowerCase();
+          }
+        }
+        else{
+          content = el.querySelector(".city-name span").textContent.toLowerCase();
+        }
+        return content == inputVal.toLowerCase();
+      });
+  
+      if(arrayFilter.length > 0){
+        errorMsg.innerHTML = `You already know the weather for ${arrayFilter[0].querySelector(".city-name span").textContent}...otherwise be more specific by providing the country code as well 😉`;
+        inputElement.value = "";
+        return;
+      }
+    }
 
     const li = document.createElement("li");
     li.classList.add("city");
@@ -28,10 +53,10 @@ function GetWeather(){
     `;
 
     li.innerHTML = markup;
-    document.getElementById('cities').appendChild(li);
+    list.appendChild(li);
   })
   .catch(() => {
-      document.getElementsByClassName('error-msg').textContent = "Enter a valid city.";
+    errorMsg.innerHTML = "Enter a valid city.";
   });
 }
 
@@ -40,17 +65,6 @@ const formBtn = document.getElementById('weather-formBtn');
 formBtn.addEventListener('click', e => {
   e.preventDefault();  
   GetWeather();
-  document.getElementsByClassName('error-msg').textContent = "";
+  errorMsg.innerHTML = "";
   inputElement.value = "";
 });
-
-
-// inputElement.addEventListener('keydown', e => {
-//   if (e.keyCode === 13) {
-//     GetWeather();
-//   }
-// });
-
-function CheckForDuplicates(){
-  
-}

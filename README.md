2acc065d13650ba801ed18458c9e0394bcdafa4f
Amir <96391496+L0rdix@users.noreply.github.com>
2023-04-09 13:33:53 +0200

    Code Cleanup plus added screenshot to readme

--- README.md
+++ README.md
@@ -1,8 +1,13 @@
 # Globecast
+
 Globecast is a little side project of mine in which i used the [Openweatermap API](https://openweathermap.org/) to get the current weather of a city.
 
 ## Use Case
+
 Globecast can be used to get the temperature and the weather of a certain city or country and compare them with other locations.
 
 ## Live Site
+
 You can access Globecast [here](https://l0rdix.github.io/Globecast/).
+
+![](./screenshot.png)
--- assets/js/index.js
+++ assets/js/index.js
@@ -1,46 +1,54 @@
-let inputElement = document.querySelector('[data-input]');
-const list = document.getElementById('cities');
-const errorMsg = document.getElementById('error-msg');
+let inputElement = document.querySelector("[data-input]");
+const list = document.getElementById("cities");
+const errorMsg = document.getElementById("error-msg");
 
-
-function GetWeather(){
+function GetWeather() {
   let inputVal = inputElement.value;
 
-  fetch(`https://api.openweathermap.org/data/2.5/weather?q=${inputVal}&APPID=692aecf0eca12b1e8b1b72c5e62db76f&units=metric`)
-  .then(response => response.json())
-  .then(data => {
-    const icon = `https://openweathermap.org/img/wn/${data.weather[0]["icon"]}@2x.png`;
-    const listItems = list.querySelectorAll(".bottom .city");
-    const listItemsArray = Array.from(listItems);
-  
-    if(listItemsArray.length > 0){
-      const arrayFilter = listItemsArray.filter(el => {
-        let content = "";
-  
-        if(inputVal.includes(",")){
-          if(inputVal.split(",")[1].length > 2){
-            inputVal = inputVal.split(",")[0];
-            content = el.querySelector(".city-name span").textContent.toLowerCase();
-          }else{
-            content = el.querySelector(".city-name").textContent.toLowerCase();
-          }
-        }
-        else{
-          content = el.querySelector(".city-name span").textContent.toLowerCase();
-        }
-        return content == inputVal.toLowerCase();
-      });
-  
-      if(arrayFilter.length > 0){
-        errorMsg.innerHTML = `You already know the weather for ${arrayFilter[0].querySelector(".city-name span").textContent}. Try searching for another city or provide a country code.`;
-        inputElement.value = "";
-        return;
-      }
-    }
+  fetch(
+    `https://api.openweathermap.org/data/2.5/weather?q=${inputVal}&APPID=692aecf0eca12b1e8b1b72c5e62db76f&units=metric`
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      const icon = `https://openweathermap.org/img/wn/${data.weather[0]["icon"]}@2x.png`;
+      const listItems = list.querySelectorAll(".bottom .city");
+      const listItemsArray = Array.from(listItems);
 
-    const li = document.createElement("li");
-    li.classList.add("city");
-    const markup = `
+      if (listItemsArray.length > 0) {
+        const arrayFilter = listItemsArray.filter((el) => {
+          let content = "";
+
+          if (inputVal.includes(",")) {
+            if (inputVal.split(",")[1].length > 2) {
+              inputVal = inputVal.split(",")[0];
+              content = el
+                .querySelector(".city-name span")
+                .textContent.toLowerCase();
+            } else {
+              content = el
+                .querySelector(".city-name")
+                .textContent.toLowerCase();
+            }
+          } else {
+            content = el
+              .querySelector(".city-name span")
+              .textContent.toLowerCase();
+          }
+          return content == inputVal.toLowerCase();
+        });
+
+        if (arrayFilter.length > 0) {
+          errorMsg.innerHTML = `You already know the weather for ${
+            arrayFilter[0].querySelector(".city-name span").textContent
+          }. Try searching for another city or provide a country code.`;
+          inputElement.value = "";
+          return;
+        }
+      }
+
+      const li = document.createElement("li");
+      li.classList.add("city");
+      const markup = `
     <h2 class="city-name" data-name="${data.name},${data.sys.country}">
     <span>${data.name}</span>
     <sup>${data.sys.country}</sup>
@@ -53,17 +61,17 @@ function GetWeather(){
     </figure>
     `;
 
-    li.innerHTML = markup;
-    list.appendChild(li);
-  })
-  .catch(() => {
-    errorMsg.innerHTML = "Enter a valid city.";
-  });
+      li.innerHTML = markup;
+      list.appendChild(li);
+    })
+    .catch(() => {
+      errorMsg.innerHTML = "Enter a valid city.";
+    });
 }
 
-const formBtn = document.getElementById('weather-formBtn');
-formBtn.addEventListener('click', e => {
-  e.preventDefault();  
+const formBtn = document.getElementById("weather-formBtn");
+formBtn.addEventListener("click", (e) => {
+  e.preventDefault();
   GetWeather();
   errorMsg.innerHTML = "";
   inputElement.value = "";
